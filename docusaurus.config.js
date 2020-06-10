ea83b0c782faba13eaf03fb15673abffef414fdd
Matevz Golob <matevz.golob@agiledrop.com>
2020-06-10 10:47:05 +0200

    Fix editUrl for modifying snippets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,86 +30,24 @@ module.exports = {
           position: 'left',
         },
         {
+          to: 'contribute/',
+          label: "Contribute",
+          position: 'right',
+        },
+        {
           href: 'https://github.com/AGILEDROP/resources/',
           label: 'Github',
           position: 'right',
         },
       ],
     },
-    // footer: {
-    //   style: 'dark',
-    //   links: [
-    //     {
-    //       title: 'Docs',
-    //       items: [
-    //         {
-    //           label: 'Style Guide',
-    //           to: 'docs/',
-    //         },
-    //         {
-    //           label: 'Second Doc',
-    //           to: 'docs/doc2/',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Community',
-    //       items: [
-    //         {
-    //           label: 'Stack Overflow',
-    //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-    //         },
-    //         {
-    //           label: 'Discord',
-    //           href: 'https://discordapp.com/invite/docusaurus',
-    //         },
-    //         {
-    //           label: 'Twitter',
-    //           href: 'https://twitter.com/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'More',
-    //       items: [
-    //         {
-    //           label: 'Blog',
-    //           to: 'blog',
-    //         },
-    //         {
-    //           label: 'GitHub',
-    //           href: 'https://github.com/facebook/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    // },
   },
-  presets: [
-    // [
-    //   '@docusaurus/preset-classic',
-    //   {
-    //     docs: {
-    //       routeBasePath: '/',
-    //       // It is recommended to set document id as docs home page (`docs/` path).
-    //       homePageId: 'db_stuff',
-    //       sidebarPath: require.resolve('./sidebars.js'),
-    //       // Please change this to your repo.
-    //       editUrl:
-    //         'https://bitbucket.org/agiledrop/developer_resources/src/master',
-    //     },
-    //     theme: {
-    //       customCss: require.resolve('./src/css/custom.scss'),
-    //     },
-    //   },
-    // ],
-  ],
+  presets: [],
   plugins: [
     [
       '@docusaurus/plugin-content-docs',
       {
-        editUrl: 'https://bitbucket.org/agiledrop/developer_resources/src/master',
+        editUrl: 'https://github.com/AGILEDROP/resources/blob/master',
         sidebarPath: require.resolve('./sidebars.js'),
       }
     ],
