b8d781922d2b1655b64634fe5c4d548bcb2b1d3f
Matevz Golob <matevz.golob@agiledrop.com>
2020-07-03 10:17:55 +0200

    Include plugin for sitemap generation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,45 +1,45 @@
 const path = require("path");
 
 module.exports = {
-  title: 'Agiledrop Developer Resources',
-  tagline: 'Code snippets and guides for web developers',
-  url: 'https://resources.agiledrop.com',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'agiledrop', // Usually your GitHub org/user name.
-  projectName: 'developer-resources', // Usually your repo name.
-    customFields: {
+  title: "Agiledrop Developer Resources",
+  tagline: "Code snippets and guides for web developers",
+  url: "https://resources.agiledrop.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "agiledrop", // Usually your GitHub org/user name.
+  projectName: "developer-resources", // Usually your repo name.
+  customFields: {
     metadata: require("./metadata"),
   },
   themeConfig: {
     navbar: {
-      title: 'Agiledrop',
+      title: "Agiledrop",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       links: [
         {
-          to: 'docs/',
-          label: 'Snippets',
-          position: 'left',
-          activeBaseRegex: 'docs/(?!contribute)'
+          to: "docs/",
+          label: "Snippets",
+          position: "left",
+          activeBaseRegex: "docs/(?!contribute)",
         },
         {
-          to: 'guides/',
+          to: "guides/",
           label: "Guides",
-          position: 'left',
+          position: "left",
         },
         {
-          to: 'docs/contribute',
+          to: "docs/contribute",
           label: "Contribute",
-          position: 'right',
-          activeBaseRegex: 'docs/contribute'
+          position: "right",
+          activeBaseRegex: "docs/contribute",
         },
         {
-          href: 'https://github.com/AGILEDROP/resources/',
-          label: 'Github',
-          position: 'right',
+          href: "https://github.com/AGILEDROP/resources/",
+          label: "Github",
+          position: "right",
         },
       ],
     },
@@ -47,22 +47,30 @@ module.exports = {
   presets: [],
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        editUrl: 'https://github.com/AGILEDROP/resources/blob/master',
-        sidebarPath: require.resolve('./sidebars.js'),
-      }
+        editUrl: "https://github.com/AGILEDROP/resources/blob/master",
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
     ],
-    ['@docusaurus/plugin-content-pages', {}],
-    path.resolve(__dirname, './plugins/guides'),
-    ['docusaurus-plugin-sass']
+    ["@docusaurus/plugin-content-pages", {}],
+    path.resolve(__dirname, "./plugins/guides"),
+    ["docusaurus-plugin-sass"],
+    [
+      "@docusaurus/plugin-sitemap",
+      {
+        cacheTime: 600 * 1000,
+        changefreq: "weekly",
+        priority: 0.5,
+      },
+    ],
   ],
   themes: [
     [
-      '@docusaurus/theme-classic',
+      "@docusaurus/theme-classic",
       {
-        customCss: require.resolve('./src/css/custom.scss'),
+        customCss: require.resolve("./src/css/custom.scss"),
       },
-    ]
-  ]
+    ],
+  ],
 };
